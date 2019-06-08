f335d1466788d3e6a6acd9df30254445e7e37753
Philippe Groarke <philippe.groarke@gmail.com>
2019-06-08 15:10:20 -0400

    string : Fix MSVC linking issue.

--- include/fea_utils/string.hpp
+++ include/fea_utils/string.hpp
@@ -34,6 +34,7 @@
 #pragma once
 #include <algorithm>
 #include <codecvt>
+#include <locale>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -137,6 +138,36 @@ inline void replace_all(std::string& out, const std::string& search,
 
 // @Brent
 // https://stackoverflow.com/questions/38688417/utf-conversion-functions-in-c11
+
+#if defined(_MSC_VER) // std::locale::id link error
+std::string to_utf8(const std::u16string& s) {
+	std::wstring_convert<std::codecvt_utf8<int16_t>, int16_t> convert;
+	auto p = reinterpret_cast<const int16_t*>(s.data());
+	return convert.to_bytes(p, p + s.size());
+}
+
+std::string to_utf8(const std::u32string& s) {
+	std::wstring_convert<std::codecvt_utf8<int32_t>, int32_t> convert;
+	auto p = reinterpret_cast<const int32_t*>(s.data());
+	return convert.to_bytes(p, p + s.size());
+}
+
+std::u16string to_utf16(const std::string& s) {
+	std::wstring_convert<std::codecvt_utf8<int16_t>, int16_t> convert;
+	auto asInt = convert.from_bytes(s);
+	return std::u16string(
+			reinterpret_cast<char16_t const*>(asInt.data()), asInt.length());
+}
+
+std::u32string to_utf32(const std::string& s) {
+	std::wstring_convert<std::codecvt_utf8<int32_t>, int32_t> convert;
+	auto asInt = convert.from_bytes(s);
+	return std::u32string(
+			reinterpret_cast<char32_t const*>(asInt.data()), asInt.length());
+}
+
+#else
+
 inline std::string to_utf8(const std::u16string& s) {
 	std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> conv;
 	return conv.to_bytes(s);
@@ -156,6 +187,7 @@ inline std::u32string to_utf32(const std::string& s) {
 	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
 	return conv.from_bytes(s);
 }
+#endif
 
 inline std::u16string to_utf16(const std::u32string& s) {
 	return to_utf16(to_utf8(s));
