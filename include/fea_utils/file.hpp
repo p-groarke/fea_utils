5e3c812f79f728306c1e514961b50cef3fd72a11
Philippe Groarke <philippe.groarke@gmail.com>
2020-03-21 16:01:17 -0400

    Add support for wide strings in file + unit tests.

--- include/fea_utils/file.hpp
+++ include/fea_utils/file.hpp
@@ -38,17 +38,35 @@
 #include <string>
 
 namespace fea {
+// Returns the executable's directory. You must provide argv[0].
+inline std::filesystem::path executable_dir(const char* argv0) {
+	return std::filesystem::absolute(argv0).remove_filename();
+}
 
-template <class Func>
-inline bool read_text_file(
-		const std::filesystem::path& fpath, const Func& func) {
-	std::ifstream ifs(fpath);
+
+// Returns the full size of the filestream. Leaves the stream at the beginning.
+template <class IFStream>
+size_t file_size(IFStream& ifs) {
+	if (!ifs.is_open()) {
+		return 0;
+	}
+
+	ifs.seekg(0, ifs.end);
+	auto ret = ifs.tellg();
+	ifs.seekg(0, ifs.beg);
+	return size_t(ret);
+}
+
+
+template <class IFStream, class String, class Func>
+bool basic_read_text_file(const std::filesystem::path& fpath, Func func) {
+	IFStream ifs(fpath);
 	if (!ifs.is_open()) {
 		fprintf(stderr, "Couldn't open file : %s\n", fpath.string().c_str());
 		return false;
 	}
 
-	std::string line;
+	String line;
 	while (std::getline(ifs, line)) {
 		if (line.size() > 0 && line.back() == '\r') {
 			line.pop_back();
@@ -58,85 +76,168 @@ inline bool read_text_file(
 	return true;
 }
 
-inline bool open_text_file(
-		const std::filesystem::path& fpath, std::vector<uint8_t>& out) {
-	std::ifstream ifs(fpath, std::ios::ate);
+// Calls your function for every line in a text file. Removes linefeeds.
+// Pass in void(std::string&&)
+template <class Func>
+bool read_text_file(const std::filesystem::path& fpath, Func func) {
+	return basic_read_text_file<std::ifstream, std::string>(fpath, func);
+}
+// Calls your function for every line in a text file. Removes linefeeds.
+// Pass in void(std::wstring&&)
+template <class Func>
+bool wread_text_file(const std::filesystem::path& fpath, Func func) {
+	return basic_read_text_file<std::wifstream, std::wstring>(fpath, func);
+}
+
+
+template <class IFStream, class String, class UInt>
+bool basic_open_text_file(
+		const std::filesystem::path& fpath, std::vector<UInt>& out) {
+	IFStream ifs(fpath, std::ios::ate);
 	if (!ifs.is_open()) {
 		fprintf(stderr, "Couldn't open file : %s\n", fpath.string().c_str());
 		return false;
 	}
 
-	std::streampos size = ifs.tellg();
 	out = {};
-	out.reserve(size_t(size));
-	ifs.seekg(0, std::ios::beg);
+	out.reserve(file_size(ifs));
 
-	std::string line;
+	String line;
 	while (std::getline(ifs, line)) {
 		if (line.size() > 0 && line.back() == '\r') {
 			line.pop_back();
 		}
 
-		for (char c : line) {
-			out.push_back(static_cast<uint8_t>(c));
+		for (auto c : line) {
+			out.push_back(static_cast<UInt>(c));
 		}
 	}
 	return true;
 }
 
-inline bool open_text_file(const std::filesystem::path& fpath,
-		std::vector<std::vector<uint8_t>>& out) {
+// Opens the text file as unsigned, and stores it in out.
+// The output is a text blob (no linefeeds).
+inline bool open_text_file(
+		const std::filesystem::path& fpath, std::vector<uint8_t>& out) {
+	return basic_open_text_file<std::ifstream, std::string>(fpath, out);
+}
+// Opens the text file as unsigned, and stores it in out.
+// The output is a text blob (no linefeeds).
+inline bool wopen_text_file(
+		const std::filesystem::path& fpath, std::vector<uint16_t>& out) {
+	return basic_open_text_file<std::wifstream, std::wstring>(fpath, out);
+}
+
+
+template <class IFStream, class String, class UInt>
+bool basic_open_text_file(const std::filesystem::path& fpath,
+		std::vector<std::vector<UInt>>& out) {
 	out = {};
 
-	return read_text_file(fpath, [&](std::string&& line) {
+	return basic_read_text_file<IFStream, String>(fpath, [&](String&& line) {
 		out.push_back({});
-		for (char c : line) {
-			out.back().push_back(static_cast<uint8_t>(c));
+		for (auto c : line) {
+			out.back().push_back(static_cast<UInt>(c));
 		}
 	});
 }
 
+// Opens text file and files the vector of vector with each line converted to
+// unsigned.
+inline bool open_text_file(const std::filesystem::path& fpath,
+		std::vector<std::vector<uint8_t>>& out) {
+	return basic_open_text_file<std::ifstream, std::string>(fpath, out);
+}
+// Opens text file and files the vector of vector with each line converted to
+// unsigned.
+inline bool wopen_text_file(const std::filesystem::path& fpath,
+		std::vector<std::vector<uint16_t>>& out) {
+	return basic_open_text_file<std::wifstream, std::wstring>(fpath, out);
+}
 
-inline bool open_text_file(
-		const std::filesystem::path& fpath, std::string& out) {
-	std::vector<uint8_t> data;
-	bool ret = open_text_file(fpath, data);
+
+template <class IFStream, class UInt, class String>
+bool basic_open_text_file(const std::filesystem::path& fpath, String& out) {
+	std::vector<UInt> data;
+	if (!basic_open_text_file<IFStream, String>(fpath, data)) {
+		return false;
+	}
 
 	out = {};
 	out.reserve(data.size());
-	for (uint8_t u : data) {
-		out.push_back(static_cast<char>(u));
+	for (UInt u : data) {
+		out.push_back(static_cast<typename String::value_type>(u));
 	}
-	return ret;
+	return true;
 }
 
+// Opens the text file and stores it in out.
+// The output is a text blob (no linefeeds).
+inline bool open_text_file(
+		const std::filesystem::path& fpath, std::string& out) {
+	return basic_open_text_file<std::ifstream, uint8_t>(fpath, out);
+}
+// Opens the text file and stores it in out.
+// The output is a text blob (no linefeeds).
+inline bool wopen_text_file(
+		const std::filesystem::path& fpath, std::wstring& out) {
+	return basic_open_text_file<std::wifstream, uint16_t>(fpath, out);
+}
+
+
+template <class IFStream, class String, class Alloc,
+		template <class, class> class Vec>
+bool basic_open_text_file(
+		const std::filesystem::path& fpath, Vec<String, Alloc>& out) {
+	out = {};
+	return basic_read_text_file<IFStream, String>(
+			fpath, [&](String&& line) { out.push_back(line); });
+}
+
+// Opens the text file and each line in the vector.
 inline bool open_text_file(
 		const std::filesystem::path& fpath, std::vector<std::string>& out) {
-	out = {};
-
-	return read_text_file(
-			fpath, [&](std::string&& line) { out.push_back(line); });
+	return basic_open_text_file<std::ifstream>(fpath, out);
+}
+// Opens the text file and each line in the vector.
+inline bool wopen_text_file(
+		const std::filesystem::path& fpath, std::vector<std::wstring>& out) {
+	return basic_open_text_file<std::wifstream>(fpath, out);
 }
 
-inline bool open_text_file_raw(
-		const std::filesystem::path& fpath, std::string& out) {
-	std::ifstream ifs(fpath, std::ios::ate);
+
+template <class IFStream, class String>
+bool open_text_file_raw(const std::filesystem::path& fpath, String& out) {
+	IFStream ifs(fpath, std::ios::ate);
 	if (!ifs.is_open()) {
 		fprintf(stderr, "Couldn't open file : %s\n", fpath.string().c_str());
 		return false;
 	}
 
-	std::streampos size = ifs.tellg();
-	out = std::string(size_t(size), '\0');
-	ifs.seekg(0, std::ios::beg);
-	ifs.read(out.data(), size);
-	size_t pos = out.find('\0');
-	if (pos != std::string::npos) {
+	out = String(file_size(ifs), '\0');
+	ifs.read(out.data(), out.size());
+
+	size_t pos = out.find('\0'); // The real end is always screwed up.
+	if (pos != String::npos) {
 		out.resize(pos);
 	}
 	return true;
 }
 
+// Opens the text file as-is (without parsing) and stores it in out.
+// Fastest option.
+inline bool open_text_file_raw(
+		const std::filesystem::path& fpath, std::string& out) {
+	return open_text_file_raw<std::ifstream>(fpath, out);
+}
+// Opens the text file as-is (without parsing) and stores it in out.
+// Fastest option.
+inline bool wopen_text_file_raw(
+		const std::filesystem::path& fpath, std::wstring& out) {
+	return open_text_file_raw<std::wifstream>(fpath, out);
+}
+
+// Opens binary file and stores bytes in vector.
 inline bool open_binary_file(
 		const std::filesystem::path& f, std::vector<uint8_t>& out) {
 	std::ifstream ifs{ f, std::ios::binary | std::ios::ate };
@@ -145,10 +246,8 @@ inline bool open_binary_file(
 		return false;
 	}
 
-	std::streampos size = ifs.tellg();
-	out = std::vector<uint8_t>(size_t(size));
-	ifs.seekg(0, std::ios::beg);
-	ifs.read(reinterpret_cast<char*>(out.data()), size);
+	out = std::vector<uint8_t>(file_size(ifs));
+	ifs.read(reinterpret_cast<char*>(out.data()), out.size());
 	return true;
 }
 } // namespace fea
--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,8 @@
 #include <gtest/gtest.h>
 
 namespace {
+std::filesystem::path exe_path;
+
 TEST(str, basics) {
 	std::string str = "a string weeee, bang, ding, ow";
 	EXPECT_TRUE(fea::contains(str, "ding"));
@@ -68,9 +70,230 @@ TEST(scope, basics) {
 	}
 	EXPECT_EQ(test_var, 1u);
 }
+
+TEST(file, basics) {
+	std::filesystem::path testfiles_dir = exe_path / "tests_data/";
+	for (const std::filesystem::path& filepath :
+			std::filesystem::directory_iterator(testfiles_dir)) {
+		{
+			size_t counter = 0;
+			fea::read_text_file(filepath, [&](std::string&& line) {
+				switch (counter) {
+				case 0: {
+					EXPECT_EQ(line, "Line1");
+				} break;
+				case 1: {
+					EXPECT_EQ(line, "Line2");
+				} break;
+				case 2: {
+					EXPECT_TRUE(line.empty());
+					EXPECT_EQ(line, "");
+				} break;
+				case 3: {
+					EXPECT_EQ(line, "Line4");
+				} break;
+				default: {
+				} break;
+				}
+
+				++counter;
+			});
+
+			EXPECT_EQ(counter, 4);
+		}
+
+		{
+			size_t counter = 0;
+			fea::wread_text_file(filepath, [&](std::wstring&& line) {
+				switch (counter) {
+				case 0: {
+					EXPECT_EQ(line, L"Line1");
+				} break;
+				case 1: {
+					EXPECT_EQ(line, L"Line2");
+				} break;
+				case 2: {
+					EXPECT_TRUE(line.empty());
+					EXPECT_EQ(line, L"");
+				} break;
+				case 3: {
+					EXPECT_EQ(line, L"Line4");
+				} break;
+				default: {
+				} break;
+				}
+
+				++counter;
+			});
+
+			EXPECT_EQ(counter, 4);
+		}
+
+		{
+			std::vector<uint8_t> text;
+			fea::open_text_file(filepath, text);
+			EXPECT_FALSE(text.empty());
+
+			const char* testagainst = "Line1Line2Line4";
+			std::vector<uint8_t> tester{ testagainst,
+				testagainst + std::strlen(testagainst) };
+			EXPECT_EQ(text.size(), tester.size());
+			EXPECT_EQ(text, tester);
+		}
+
+		{
+			std::vector<uint16_t> text;
+			fea::wopen_text_file(filepath, text);
+			EXPECT_FALSE(text.empty());
+
+			const wchar_t* testagainst = L"Line1Line2Line4";
+			std::vector<uint16_t> tester{ testagainst,
+				testagainst + std::wcslen(testagainst) };
+			EXPECT_EQ(text.size(), tester.size());
+			EXPECT_EQ(text, tester);
+		}
+
+		{
+			std::vector<std::vector<uint8_t>> lines;
+			fea::open_text_file(filepath, lines);
+			EXPECT_FALSE(lines.empty());
+
+			std::vector<std::vector<uint8_t>> tester;
+
+			const char* line1 = "Line1";
+			const char* line2 = "Line2";
+			const char* line4 = "Line4";
+			tester.push_back({ line1, line1 + std::strlen(line1) });
+			tester.push_back({ line2, line2 + std::strlen(line2) });
+			tester.push_back({});
+			tester.push_back({ line4, line4 + std::strlen(line4) });
+
+			ASSERT_EQ(lines.size(), tester.size());
+			for (size_t i = 0; i < lines.size(); ++i) {
+				EXPECT_EQ(lines[i].size(), tester[i].size());
+				EXPECT_EQ(lines[i], tester[i]);
+			}
+		}
+
+		{
+			std::vector<std::vector<uint16_t>> lines;
+			fea::wopen_text_file(filepath, lines);
+			EXPECT_FALSE(lines.empty());
+
+			std::vector<std::vector<uint16_t>> tester;
+
+			const wchar_t* line1 = L"Line1";
+			const wchar_t* line2 = L"Line2";
+			const wchar_t* line4 = L"Line4";
+			tester.push_back({ line1, line1 + std::wcslen(line1) });
+			tester.push_back({ line2, line2 + std::wcslen(line2) });
+			tester.push_back({});
+			tester.push_back({ line4, line4 + std::wcslen(line4) });
+
+			ASSERT_EQ(lines.size(), tester.size());
+			for (size_t i = 0; i < lines.size(); ++i) {
+				EXPECT_EQ(lines[i].size(), tester[i].size());
+				EXPECT_EQ(lines[i], tester[i]);
+			}
+		}
+
+		{
+			std::string text;
+			fea::open_text_file(filepath, text);
+			EXPECT_FALSE(text.empty());
+
+			std::string tester = "Line1Line2Line4";
+			EXPECT_EQ(text.size(), tester.size());
+			EXPECT_EQ(text, tester);
+		}
+		{
+			std::wstring text;
+			fea::wopen_text_file(filepath, text);
+			EXPECT_FALSE(text.empty());
+
+			std::wstring tester = L"Line1Line2Line4";
+			EXPECT_EQ(text.size(), tester.size());
+			EXPECT_EQ(text, tester);
+		}
+
+		{
+			std::vector<std::string> lines;
+			fea::open_text_file(filepath, lines);
+			EXPECT_FALSE(lines.empty());
+
+			std::vector<std::string> tester;
+
+			tester.push_back("Line1");
+			tester.push_back("Line2");
+			tester.push_back("");
+			tester.push_back("Line4");
+
+			ASSERT_EQ(lines.size(), tester.size());
+			for (size_t i = 0; i < lines.size(); ++i) {
+				EXPECT_EQ(lines[i].size(), tester[i].size());
+				EXPECT_EQ(lines[i], tester[i]);
+			}
+		}
+		{
+			std::vector<std::wstring> lines;
+			fea::wopen_text_file(filepath, lines);
+			EXPECT_FALSE(lines.empty());
+
+			std::vector<std::wstring> tester;
+
+			tester.push_back(L"Line1");
+			tester.push_back(L"Line2");
+			tester.push_back(L"");
+			tester.push_back(L"Line4");
+
+			ASSERT_EQ(lines.size(), tester.size());
+			for (size_t i = 0; i < lines.size(); ++i) {
+				EXPECT_EQ(lines[i].size(), tester[i].size());
+				EXPECT_EQ(lines[i], tester[i]);
+			}
+		}
+
+		{
+			std::string text;
+			fea::open_text_file_raw(filepath, text);
+			EXPECT_FALSE(text.empty());
+
+			std::string tester = "Line1\nLine2\n\nLine4";
+			EXPECT_EQ(text.size(), tester.size());
+			EXPECT_EQ(text, tester);
+		}
+		{
+			std::wstring text;
+			fea::wopen_text_file_raw(filepath, text);
+			EXPECT_FALSE(text.empty());
+
+			std::wstring tester = L"Line1\nLine2\n\nLine4";
+			EXPECT_EQ(text.size(), tester.size());
+			EXPECT_EQ(text, tester);
+		}
+
+		{
+			const char* testagainst = "Line1\nLine2\n\nLine4";
+			if (filepath.string().find("crlf") != std::string::npos) {
+				testagainst = "Line1\r\nLine2\r\n\r\nLine4";
+			}
+
+			std::vector<uint8_t> bytes;
+			fea::open_binary_file(filepath, bytes);
+			EXPECT_FALSE(bytes.empty());
+
+			std::vector<uint8_t> tester{ testagainst,
+				testagainst + std::strlen(testagainst) };
+			EXPECT_EQ(bytes.size(), tester.size());
+			EXPECT_EQ(bytes, tester);
+		}
+	}
+}
 } // namespace
 
 int main(int argc, char** argv) {
+	exe_path = fea::executable_dir(argv[0]);
+
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
