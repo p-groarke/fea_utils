ef61bf0251581dcc7d311b6f31bd857aba701d64
Philippe Groarke <philippe.groarke@gmail.com>
2020-04-25 13:05:06 -0400

    Add any to utf8 and vice versa.

--- include/fea_utils/string.hpp
+++ include/fea_utils/string.hpp
@@ -360,6 +360,41 @@ inline std::wstring utf32_to_ucs2_w(const std::u32string& s) {
 	return utf8_to_ucs2_w(utf32_to_utf8(s));
 }
 
+
+// Useful generalized conversions
+
+template <class CharT>
+std::string any_to_ut8(const m_string<CharT>& str) {
+	if constexpr (std::is_same_v<CharT, char>) {
+		return str;
+	} else if constexpr (std::is_same_v<CharT, wchar_t>) {
+		return utf16_to_utf8(str);
+	} else if constexpr (std::is_same_v<CharT, char16_t>) {
+		return utf16_to_utf8(str);
+	} else if constexpr (std::is_same_v<CharT, char32_t>) {
+		return utf32_to_utf8(str);
+	} else {
+		assert(false);
+		throw std::runtime_error{ "any_to_utf8 : unsupported string type" };
+	}
+}
+
+template <class CharT>
+m_string<CharT> utf8_to_any(const std::string& str) {
+	if constexpr (std::is_same_v<CharT, char>) {
+		return str;
+	} else if constexpr (std::is_same_v<CharT, wchar_t>) {
+		return utf8_to_utf16_w(str);
+	} else if constexpr (std::is_same_v<CharT, char16_t>) {
+		return utf8_to_utf16(str);
+	} else if constexpr (std::is_same_v<CharT, char32_t>) {
+		return utf8_to_utf32(str);
+	} else {
+		assert(false);
+		throw std::runtime_error{ "utf8_to_any : unsupported string type" };
+	}
+}
+
 template <class CharT>
 std::u32string any_to_utf32(const m_string<CharT>& str) {
 	if constexpr (std::is_same_v<CharT, char>) {
@@ -369,7 +404,7 @@ std::u32string any_to_utf32(const m_string<CharT>& str) {
 	} else if constexpr (std::is_same_v<CharT, char16_t>) {
 		return utf16_to_utf32(str);
 	} else if constexpr (std::is_same_v<CharT, char32_t>) {
-		return std::u32string{ str };
+		return str;
 	} else {
 		assert(false);
 		throw std::runtime_error{ "any_to_utf32 : unsupported string type" };
